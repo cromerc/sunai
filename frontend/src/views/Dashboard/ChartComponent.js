12c1b59084bbe62e9654a1bfb61467450db77ec9
Chris Cromer <chris@cromer.cl>
2022-11-11 18:46:33 -0300

    finish up styling of dashboard view

--- frontend/src/views/Dashboard/ChartComponent.js
+++ frontend/src/views/Dashboard/ChartComponent.js
@@ -1,9 +1,61 @@
 import React from "react";
 
-export default function ChartComponent() {
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip,
+    Legend
+);
+
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top',
+        }
+    },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+    labels,
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: [1, 4, 10],
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+        {
+            label: 'Dataset 2',
+            data: [1, 2, 3],
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+    ],
+};
+
+export default function ChartComponent({ data1, data2 }) {
+    data.datasets[0].data = data1.y;
+    data.datasets[1].data = data2.y;
+
     return (
-        <div>
-            test
+        <div className="w-full">
+            <Line options={options} data={data} />
         </div>
     );
 };
--- frontend/src/views/Dashboard/DashboardComponent.js
+++ frontend/src/views/Dashboard/DashboardComponent.js
@@ -1,16 +1,37 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { FileUpload } from 'primereact/fileupload';
 import { Toast } from 'primereact/toast';
 import { Panel } from 'primereact/panel';
+import { Card } from 'primereact/card';
 
 import ChartComponent from './ChartComponent';
+
+class Coords {
+    x = [];
+    y = [];
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+};
 
 export default function DashboardComponent() {
     const toast = useRef(null);
 
-    const onBasicUpload = () => {
-            toast.current.show({severity: 'info', summary: 'Success', detail: 'File Uploaded'});
-    }
+    const [sum, setSum] = useState(0);
+    const [max, setMax] = useState(0);
+
+    const [data1, setData1] = useState(new Coords());
+    const [data2, setData2] = useState(new Coords());
+
+    const onUpload = () => {
+        toast.current.show({ severity: 'info', summary: 'Éxito', detail: 'File Uploaded' });
+    };
+
+    const chartData = () => {
+
+    };
 
     return (
         <div className="card py-4 px-8">
@@ -24,10 +45,24 @@ export default function DashboardComponent() {
                                     url="api/excel"
                                     accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                                     maxFileSize={1000000}
-                                    onUpload={onBasicUpload} />
+                                    onUpload={onUpload} />
                         </div>
-                        <div>
-                            <ChartComponent />
+                        <div className="grid">
+                            <div className="flex justify-content-center col-6">
+                                <Card title="Chart" style={{ textAlign: 'center' }} className='w-9'>
+                                    <ChartComponent data1={data1} data2={data2} />
+                                </Card>
+                            </div>
+                            <div className="flex justify-content-center col-6">
+                                <div className="grid">
+                                    <div className="flex justify-content-center col-12">
+                                        <Card title="Sum" style={{ textAlign: 'center', width: '25rem', marginBottom: '2em' }}>{sum}</Card>
+                                    </div>
+                                    <div className="flex justify-content-center col-12">
+                                        <Card title="Max" style={{ textAlign: 'center', width: '25rem', marginBottom: '2em' }}>{max}</Card>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </Panel>
                 </div>
