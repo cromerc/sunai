a5646198898ffa25e2a7349b234805d35db31ae6
Chris Cromer <chris@cromer.cl>
2022-11-11 16:11:14 -0300

    change to float type for the division

--- schema/planta_power.sql
+++ schema/planta_power.sql
@@ -3,6 +3,6 @@ create table planta_power
     id              bigint auto_increment,
     id_i            int not null,
     active_power_im int not null,
-    division        int not null,
+    division        float not null,
     primary key (id)
 );
