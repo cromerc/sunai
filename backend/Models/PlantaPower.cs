2ac4e597e32a4f8f2bd49326315f6442d4a59195
Chris Cromer <chris@cromer.cl>
2022-11-11 16:10:25 -0300

    scaffold the database

---
+++ backend/Models/PlantaPower.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend.Models
+{
+    public partial class PlantaPower
+    {
+        public long Id { get; set; }
+        public int IdI { get; set; }
+        public int ActivePowerIm { get; set; }
+        public float Division { get; set; }
+    }
+}
---
+++ backend/Models/databaseContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace backend.Models
+{
+    public partial class databaseContext : DbContext
+    {
+        public databaseContext()
+        {
+        }
+
+        public databaseContext(DbContextOptions<databaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<PlantaPower> PlantaPowers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=localhost;port=3306;user=user;password=pass;database=database", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.9.3-mariadb"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_general_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<PlantaPower>(entity =>
+            {
+                entity.ToTable("planta_power");
+
+                entity.Property(e => e.Id)
+                    .HasColumnType("bigint(20)")
+                    .HasColumnName("id");
+
+                entity.Property(e => e.ActivePowerIm)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("active_power_im");
+
+                entity.Property(e => e.Division)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("division");
+
+                entity.Property(e => e.IdI)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("id_i");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
