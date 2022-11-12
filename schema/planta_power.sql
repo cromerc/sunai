00e3a240897a22c99983dda14e6637b7f7c24a36
Chris Cromer <chris@cromer.cl>
2022-11-11 21:46:37 -0300

    add date and time

--- schema/planta_power.sql
+++ schema/planta_power.sql
@@ -2,6 +2,7 @@ create table planta_power
 (
     id              bigint auto_increment,
     id_i            int not null,
+    fecha_im        datetime not null,
     active_power_im int not null,
     division        float not null,
     primary key (id)
